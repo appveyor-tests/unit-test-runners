615efcb8778d85276c1953356ea0661a9a5c4c97
Ilya Finkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2017-03-30 21:39:20 -0700

    Update checkTestsNumber.ps1

--- checkTestsNumber.ps1
+++ checkTestsNumber.ps1
@@ -4,11 +4,13 @@ $headers = @{
   "Authorization" = "Bearer $env:API_TOKEN"
   "Content-type" = "application/json"
 }
-
+sleep 5
 $b = Invoke-RestMethod -Uri "$baseUrl/api/projects/AppVeyor/unit-test-runners" -Headers $headers  -Method GET
-if ($b.build.jobs[0].testsCount -ne $testsExpected) {
+$testActual = $b.build.jobs[0].testsCount;
+write-host $testActual
+if ($testActual -ne $testsExpected) {
     Write-Error "Tests expected $testsExpected, actual $b.build.jobs[0].testsCount"
 }
 else {
-Write-host "Tests expected $testsExpected, actual $b.build.jobs[0].testsCount" -foregroundcolor Green
+Write-host "Tests expected $testsExpected, actual $testActual" -foregroundcolor Green
 }
