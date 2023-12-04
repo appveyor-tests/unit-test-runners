5eefb1f511f5d2be48ecd50a25f2a364129fd6a5
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-12-04 15:36:23 -0800

    Create install_latest_nunit_console.ps1

---
+++ install_latest_nunit_console.ps1
@@ -0,0 +1,19 @@
+Write-Host "Installing NUnit 3.15.2..." -ForegroundColor Cyan -NoNewline
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$nunitPath = "$env:SYSTEMDRIVE\Tools\NUnit3"
+
+if (Test-Path $nunitPath) {
+    Remove-Item $nunitPath -Recurse -Force
+}
+
+
+$zipPath = "$env:TEMP\NUnit.Console.3.16.3.nupkg"
+$tempPath = "$env:TEMP\NUnit.Console"
+    (New-Object Net.WebClient).DownloadFile('https://github.com/nunit/nunit-console/releases/download/3.16.3/NUnit.ConsoleRunner.3.16.3.nupkg', $zipPath)
+7z x $zipPath -y -o"$tempPath" | Out-Null
+New-Item -Path "$nunitPath" -ItemType Directory -Force | Out-Null
+[IO.Directory]::Move("$tempPath\tools", "$nunitPath\bin")
+Copy-Item -Path "$tempPath\tools\agents" -Destination $nunitPath -Recurse
+Remove-Item $zipPath
