f7c2bd64be2b18f0b206ea6e3fe031b3131b690b
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-12-04 17:14:17 -0800

    Update install_latest_nunit_console.ps1

--- install_latest_nunit_console.ps1
+++ install_latest_nunit_console.ps1
@@ -14,6 +14,7 @@ $tempPath = "$env:TEMP\NUnit.Console"
     (New-Object Net.WebClient).DownloadFile('https://github.com/nunit/nunit-console/releases/download/3.16.3/NUnit.ConsoleRunner.3.16.3.nupkg', $zipPath)
 7z x $zipPath -y -o"$tempPath" | Out-Null
 New-Item -Path "$nunitPath" -ItemType Directory -Force | Out-Null
+New-Item -Path "$nunitPath\bin" -ItemType Directory -Force | Out-Null
 Get-ChildItem -Path "$tempPath\tools\*.*" | Move-Item -Destination "$nunitPath\bin"
 #[IO.Directory]::Move("$tempPath\tools", "$nunitPath\bin")
 Copy-Item -Path "$tempPath\tools\agents" -Destination $nunitPath -Recurse
