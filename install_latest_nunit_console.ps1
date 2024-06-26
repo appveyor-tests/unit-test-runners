12bc8bf9ce70a3df27fa6825f0862237e8446108
Feodor Fitsner <feodor@appveyor.com>
2024-06-26 10:25:12 -0700

    squashed commits

--- install_latest_nunit_console.ps1
+++ install_latest_nunit_console.ps1
@@ -1,4 +1,4 @@
-Write-Host "Installing NUnit 3.15.2..." -ForegroundColor Cyan -NoNewline
+Write-Host "Installing NUnit 3.16.3..." -ForegroundColor Cyan -NoNewline
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -14,6 +14,8 @@ $tempPath = "$env:TEMP\NUnit.Console"
     (New-Object Net.WebClient).DownloadFile('https://github.com/nunit/nunit-console/releases/download/3.16.3/NUnit.ConsoleRunner.3.16.3.nupkg', $zipPath)
 7z x $zipPath -y -o"$tempPath" | Out-Null
 New-Item -Path "$nunitPath" -ItemType Directory -Force | Out-Null
-[IO.Directory]::Move("$tempPath\tools", "$nunitPath\bin")
+New-Item -Path "$nunitPath\bin" -ItemType Directory -Force | Out-Null
+Get-ChildItem -Path "$tempPath\tools\*.*" | Move-Item -Destination "$nunitPath\bin"
+#[IO.Directory]::Move("$tempPath\tools", "$nunitPath\bin")
 Copy-Item -Path "$tempPath\tools\agents" -Destination $nunitPath -Recurse
 Remove-Item $zipPath
